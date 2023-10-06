45e4e77f1d2377a5dc0c3dfd1eff09774fa90eeb
Сагдиева Камилла Рамилевна <202115@mck-ktits.ru>
2023-10-06 12:37:50 +0300

    Добавьте файлы проекта.

---
+++ Child/ClassWork.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Child
+{
+    abstract class ClassWork
+    {
+        public abstract void Work();
+    }
+    class ReadingTask : ClassWork
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Читаю задание.");
+        }
+    }
+    class RewritingTask : ClassWork
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Переписываю задание.");
+        }
+    }
+    class ThinkingTask : ClassWork
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Думаю над заданием.");
+        }
+    }
+    class SolvingTask : ClassWork
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Решаю задание.");
+        }
+    }
+    class Student
+    {
+        public void Work(ReadingTask readingTask)
+        {
+            readingTask.Work();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadingTask readingTask = new ReadingTask();
+            readingTask.Work();
+            RewritingTask rewritingTask = new RewritingTask();
+            rewritingTask.Work();
+            ThinkingTask thinkingTask = new ThinkingTask();
+            thinkingTask.Work();
+            SolvingTask solvingTask = new SolvingTask();
+            solvingTask.Work();
+            Console.WriteLine("------------");
+            Student student = new Student();
+            student.Work(readingTask);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Child/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Child
+{
+    abstract class Reading
+    {
+        public abstract void Read();
+    }
+    class Azbuka : Reading
+    {
+        public override void Read()
+        {
+            Console.WriteLine("ABC");
+        }
+    }
+    class Writing : Reading
+    {
+        public override void Read()
+        {
+            Console.WriteLine("Пишу");
+        }
+    }
+    class Child
+    {
+        public void Read(Reading reading)
+        {
+            reading.Read();
+        }
+    }
+
+    class Program1
+    {
+        static void Main1(string[] args)
+        {
+            Azbuka azbuka = new Azbuka();
+            azbuka.Read();
+            Writing writing = new Writing();
+            writing.Read();
+            Console.WriteLine("------------");
+            Child child = new Child();
+            child.Read(azbuka);
+            Console.ReadKey();
+        }
+    }
+}
